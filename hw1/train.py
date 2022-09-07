ab467bdbb8c1537436f752733f46d9061a90656d
Aidan O'Gara <aidanogara623@gmail.com>
2022-09-07 14:34:26 -0700

    lstm implementation

--- hw1/train.py
+++ hw1/train.py
@@ -12,13 +12,7 @@ from utils import (
     build_output_tables,
 )
 
-
-def setup_dataloader(args):
-    """
-    return:
-        - train_loader: torch.utils.data.Dataloader
-        - val_loader: torch.utils.data.Dataloader
-    """
+def preprocess_data(args):
     # ================== TODO: CODE HERE ================== #
     # Task: Load the training data from provided json file.
     # Perform some preprocessing to tokenize the natural
@@ -86,23 +80,131 @@ def setup_dataloader(args):
         data_dict['actions'] = torch.Tensor(data_dict['actions']).to(torch.int64)
         data_dict['targets'] = torch.Tensor(data_dict['targets']).to(torch.int64)
 
-        # Perform one-hot encoding on all token Tensors
-        # Specify num_classes to preserve size of array across data inputs
-        data_dict['instructions'] = F.one_hot(data_dict['instructions'],
-                                              num_classes = args.vocab_size)
+        # # Perform one-hot encoding on all token Tensors
+        # # Specify num_classes to preserve size of array across data inputs
+        # data_dict['instructions'] = F.one_hot(data_dict['instructions'],
+        #                                       num_classes = args.vocab_size)
         data_dict['actions'] = F.one_hot(data_dict['actions'],
                                          num_classes = len(actions_to_index))
         data_dict['targets'] = F.one_hot(data_dict['targets'],
                                          num_classes = len(targets_to_index))
     
-    print('tokenized')
+    print('preprocessed')
+
+    return train_dict, val_dict, maps
+
+class alfred_dataset(torch.utils.data.Dataset):
+    def __init__(self, data_dict):
+        """
+        Initialize the Dataset, which has the same structure as
+        the data_dict provided by preprocess_data(), but implements
+        the necessary __len__() and __getitem__() methods. 
+        """
+        super().__init__()
+        self.instructions = data_dict['instructions']
+        self.actions = data_dict['actions']
+        self.targets = data_dict['targets']
     
-    train_loader = torch.utils.data.DataLoader(train_dict)
-    val_loader = torch.utils.data.DataLoader(val_dict)
+    def __len__(self):
+        """
+        Verify that all attributes are of the same length,
+        then return that length. 
+        """
+        assert len(self.instructions) == len(self.actions)
+        assert len(self.actions) == len(self.targets)
+        return len(self.instructions)
+
+    def __getitem__(self, idx):
+        """
+        Returns a 3-tuple of the instructions, actions,
+        and targets at the idx. 
+        """
+        return self.instructions[idx], self.actions[idx], self.targets[idx]
+
+def setup_dataloader(args):
+    """
+    return:
+        - train_loader: torch.utils.data.Dataloader
+        - val_loader: torch.utils.data.Dataloader
+    """
+    # Process the text into tokens stored in dictionaries
+    train_dict, val_dict, maps = preprocess_data(args)
+
+    # Store the tokenized data in a custom defined Dataset object
+    train_dataset = alfred_dataset(train_dict)
+    val_dataset = alfred_dataset(val_dict)
+
+    # Wrap the Dataset objects in iterable DataLoaders
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=args.batch_size)
+
     return train_loader, val_loader, maps
 
+class LSTM(torch.nn.Module):
+    def __init__(self, args, maps):
+        """
+        Defines our custom LSTM class. 
+        """
+        # The internet says I should initialize my parent class?
+        # This seems silly, shouldn't Python take care of that?
+        # Not sure, including it anyways. See here: 
+        # https://stackoverflow.com/questions/576169/understanding-python-super-with-init-methods
+        super().__init__()
 
-def setup_model(args):
+        # Initialize some hyperparameters
+        self.embedding_dim = 128
+        self.lstm_hidden_dim = 128
+        self.lstm_layers = 1
+        self.dropout = 0
+        self.bidirectional = False
+        self.action_classes = len(maps[2])
+        self.target_classes = len(maps[4])
+
+        # Initialize an embedding layer the size of our vocabulary
+        self.embed = torch.nn.Embedding(num_embeddings=args.vocab_size, 
+                                        embedding_dim=self.embedding_dim)
+
+        # Initialize an LSTM block using our hyperparameters
+        self.lstm = torch.nn.LSTM(input_size=self.embedding_dim, 
+                                  hidden_size=self.lstm_hidden_dim,
+                                  num_layers=self.lstm_layers,
+                                  dropout=self.dropout,
+                                  bidirectional=self.bidirectional,
+                                  batch_first=True)
+
+        # Initialize a fully-connected linear layer
+        self.fc_action = torch.nn.Linear(in_features=self.lstm_hidden_dim,
+                                         out_features=self.action_classes)
+        self.fc_target = torch.nn.Linear(in_features=self.lstm_hidden_dim,
+                                         out_features=self.target_classes)
+
+    def forward(self, inputs):
+        """
+        Performs a forward pass through the LSTM. 
+        """
+        # Embed the inputs
+        embeds = self.embed(inputs)
+        # TODO: Check the embedding dimension, make sure it's (len_input, batch, hidden_features)
+
+        # Run the embeddings through the LSTM
+        # TODO; You probably want the final hidden state, not each token's representation
+        lstm_output, (h_n, c_n)  = self.lstm(embeds)
+
+        # Use two separate fully connected layers to predict actions and targets
+        action_mass = self.fc_action(h_n)
+        target_mass = self.fc_target(h_n)
+
+        # Perform softmax to turn probability mass into probabilities
+        # action_probs = F.softmax(action_mass)
+        # target_probs = F.softmax(target_mass)
+
+        # return action_probs, target_probs
+
+        return action_mass.squeeze(), target_mass.squeeze()
+        
+
+
+def setup_model(args, maps, device):
     """
     return:
         - model: YourOwnModelClass
@@ -110,7 +212,8 @@ def setup_model(args):
     # ================== TODO: CODE HERE ================== #
     # Task: Initialize your model.
     # ===================================================== #
-    model = None
+
+    model = LSTM(args, maps)
     return model
 
 
@@ -125,9 +228,11 @@ def setup_optimizer(args, model):
     # Task: Initialize the loss function for action predictions
     # and target predictions. Also initialize your optimizer.
     # ===================================================== #
-    action_criterion = None
-    target_criterion = None
-    optimizer = None
+    action_criterion = torch.nn.CrossEntropyLoss()
+    target_criterion = torch.nn.CrossEntropyLoss()
+    # TODO: How do I optimize the heads separately?
+    # TODO: Do I not call softmax inside the LSTM?
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
 
     return action_criterion, target_criterion, optimizer
 
@@ -153,19 +258,19 @@ def train_epoch(
 
     # iterate over each batch in the dataloader
     # NOTE: you may have additional outputs from the loader __getitem__, you can modify this
-    for (inputs, labels) in loader:
+    for inputs, actions, targets in loader:
         # put model inputs to device
-        inputs, labels = inputs.to(device), labels.to(device)
+        inputs, actions, targets = inputs.to(device), actions.to(device), targets.to(device)
 
         # calculate the loss and train accuracy and perform backprop
         # NOTE: feel free to change the parameters to the model forward pass here + outputs
-        actions_out, targets_out = model(inputs, labels)
+        actions_out, targets_out = model(inputs)
 
         # calculate the action and target prediction loss
         # NOTE: we assume that labels is a tensor of size Bx2 where labels[:, 0] is the
         # action label and labels[:, 1] is the target label
-        action_loss = action_criterion(actions_out.squeeze(), labels[:, 0].long())
-        target_loss = target_criterion(targets_out.squeeze(), labels[:, 1].long())
+        action_loss = action_criterion(actions_out.squeeze(), actions.float())
+        target_loss = target_criterion(targets_out.squeeze(), targets.float())
 
         loss = action_loss + target_loss
 
@@ -183,12 +288,22 @@ def train_epoch(
         # NOTE: this could change depending on if you apply Sigmoid in your forward pass
         action_preds_ = actions_out.argmax(-1)
         target_preds_ = targets_out.argmax(-1)
+        actions_ = actions.argmax(-1)
+        targets_ = targets.argmax(-1)
+
+        # print('shapecheck')
+        # print(actions.shape)
+        # print(targets.shape)
+        # print(actions_out.shape)
+        # print(targets_out.shape)
+        # print(action_preds_)
+        # print(target_preds_)
 
         # aggregate the batch predictions + labels
         action_preds.extend(action_preds_.cpu().numpy())
         target_preds.extend(target_preds_.cpu().numpy())
-        action_labels.extend(labels[:, 0].cpu().numpy())
-        target_labels.extend(labels[:, 1].cpu().numpy())
+        action_labels.extend(actions_.cpu().numpy())
+        target_labels.extend(targets_.cpu().numpy())
 
     action_acc = accuracy_score(action_preds, action_labels)
     target_acc = accuracy_score(target_preds, target_labels)
@@ -283,6 +398,7 @@ def train(args, model, loaders, optimizer, action_criterion, target_criterion, d
 
 def main(args):
     device = get_device(args.force_cpu)
+    torch.manual_seed(42)
 
     # get dataloaders
     train_loader, val_loader, maps = setup_dataloader(args)
@@ -322,16 +438,17 @@ if __name__ == "__main__":
     )
     parser.add_argument("--force_cpu", action="store_true", help="debug mode")
     parser.add_argument("--eval", action="store_true", help="run eval")
-    parser.add_argument("--num_epochs", default=1000, help="number of training epochs")
+    parser.add_argument("--num_epochs", default=1000, help="number of training epochs", type=int)
     parser.add_argument(
-        "--val_every", default=5, help="number of epochs between every eval loop"
+        "--val_every", default=5, help="number of epochs between every eval loop", type=int
     )
-    parser.add_argument("--vocab_size", default=1000, help="number of tokens in vocab")
 
     # ================== TODO: CODE HERE ================== #
     # Task (optional): Add any additional command line
     # parameters you may need here
     # ===================================================== #
+    parser.add_argument("--vocab_size", default=1000, help="number of tokens in vocab")
+
     args = parser.parse_args()
 
     main(args)
